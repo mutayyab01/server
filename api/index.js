646ab1329b2a888cb0ec246d57b8eee058e86577
Mutayyab Imran <mutayyab786yo@gmail.com>
2025-05-28 22:30:27 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const saleRoute = require('../router/saleRoute');
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['*', '*'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
